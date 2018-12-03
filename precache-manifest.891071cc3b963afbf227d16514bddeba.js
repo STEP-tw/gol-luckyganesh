7c91e0d53ccfa1e0ab9b4b29c307cba5071ee80c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-03 07:13:08 +0000

    Deploy STEP-tw/gol-luckyganesh to github.com/STEP-tw/gol-luckyganesh.git:gh-pages

---
+++ precache-manifest.891071cc3b963afbf227d16514bddeba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9426853463494408042a",
+    "url": "/gol-luckyganesh/static/js/runtime~main.94268534.js"
+  },
+  {
+    "revision": "03bf7d909b22bb54d29b",
+    "url": "/gol-luckyganesh/static/js/main.03bf7d90.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/gol-luckyganesh/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "03bf7d909b22bb54d29b",
+    "url": "/gol-luckyganesh/static/css/main.fb6505e9.chunk.css"
+  },
+  {
+    "revision": "5a09e86d1833001f16a8dd0cf1f1dae4",
+    "url": "/gol-luckyganesh/index.html"
+  }
+];
